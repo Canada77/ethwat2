0c93ebdb07d8be1b67320f72ee250f66f9d66ab5
Ryan Westman <Ryan@Ryans-MacBook-Pro-2.local>
2019-11-09 23:23:56 -0500

    Added Select Handler

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import RepContract from "./contracts/RepContract.json";
 import getWeb3 from "./utils/getWeb3";
-import {Input, InputNumber, Button} from 'antd';
+import {Input, InputNumber, Button, Select} from 'antd'; //Select for handlechange
 
 import "./App.css";
 
@@ -35,6 +35,9 @@ class App extends Component {
         deployedNetwork && deployedNetwork.address,
       );
 
+      // handlechange statement
+      const {Option} = Select;
+
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
       this.setState({ web3, accounts, repContract });//, this.runExample);
@@ -59,6 +62,11 @@ class App extends Component {
   //   // Update state with the result.
   //   this.setState({ storageValue: response });
   // };
+
+  catIndex(defaultValue) {
+    console.log(`selected ${defaultValue}`);
+  }
+
   buttonClicked() {
     console.log(this.state.addressOfUserToRate + "will be rated " + this.state.rating);
   }
@@ -68,20 +76,22 @@ class App extends Component {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
-      <div className="App" >     
-        <img src="./assets/space-img.jpg" alt="" />        
+      <div className="App" >
+        <img src="./assets/space-img.jpg" alt="" />
         <h1 className="App-font" style={{color: "white", fontSize: "50pt"}}>UnRep</h1>
         <Input style={{width: "50%"}} placeholder="Address of user to rate" type="text" onChange={event => {this.setState({addressOfUserToRate: event.target.value})}} />
         <br />
         <span style={{color: "white"}}>Category:</span>
-        <select>
-          <option value="driving">Driving</option> 
-        </select>
+        <Select defaultValue="Programming">
+          <Select.Option value="Programming">Programming</Select.Option>
+          <Select.Option value="Credit">Credit</Select.Option>
+          <Select.Option value="Driving">Driving</Select.Option>
+        </Select>
         <br />
         <span style={{color: "white"}}>Reputation score out of 100:</span>
         <InputNumber min={1} max={100} style={{width: "50%"}}  onChange={value => {this.setState({rating: value})}} />
         <br />
-        <Button onClick={this.buttonClicked}>Submit Rep</Button>      
+        <Button onClick={this.buttonClicked}>Submit Rep</Button>
       </div>
     );
   }
